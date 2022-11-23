4dc4e5cff1f2161e705cc5b7e28edce0ee2e3157
Yelstin de Leon <ayrtom.omega01@gmail.com>
2022-11-23 07:29:39 -0600

    App changed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React -  Github pages
+          Learn React -  Github pages - Github actions deploy
         </a>
       </header>
     </div>
